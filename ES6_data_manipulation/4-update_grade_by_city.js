0b3322d00c843c3a0bf148ac7cc5a480452d7e16
HamzaMoinet <moinethamza0@gmail.com>
2025-04-01 10:22:00 +0200

    task 1 to 6

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default function updateStudentGradeByCity(students, city, newGrade) {
+  return students.filter((student) => student.location === city)
+  .map((student) => {
+    const gradeObj = newGrade.find((grade) => grade.studentId === student.id);
+    return {
+      ...student,
+      grade: gradeObj ? gradeObj.grade : 'N/A',
+    };
+  });
+}
---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,3 @@
+export default function setFromArray(number) {
+  return new Set(number);
+}
