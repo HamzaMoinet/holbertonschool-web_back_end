ec3a4cc1f1f49d3d05aea9cd0c33fb89641afd9e
HamzaMoinet <moinethamza0@gmail.com>
2025-04-02 14:57:36 +0200

    add README and all task finish

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const results = mathFunction();
+    queue.push(results);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
