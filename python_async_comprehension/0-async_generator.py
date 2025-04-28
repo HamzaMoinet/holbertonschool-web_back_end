072fb27fdccf6d3fc9766e3157a6cb83c9b57219
HamzaMoinet <moinethamza0@gmail.com>
2025-04-28 11:04:15 +0200

    modif comment

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
+"""
+Générateur asynchrone produisant 10 nombres flottants aléatoires entre 0 et 10,
+attendant 1 seconde avant chaque valeur produite.
+"""
 import asyncio
 import random
 
 
 async def async_generator():
-    """Coroutine qui génère 10 nombres aléatoires entre 0 et 10 avec
-    une pause d'1 seconde à chaque fois."""
+    """
+    Génère de manière asynchrone 10 nombres flottants aléatoires entre 0 et 10,
+    en attendant 1 seconde avant de produire chaque valeur.
+
+    Yields :
+        float : Nombre aléatoire entre 0 et 10.
+    """
+
     for _ in range(10):
-        await asyncio.sleep(1)  # Attendre 1 seconde de manière asynchrone
-        yield random.uniform(0, 10)  # Générer aléatoire entre 0 et 10
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
