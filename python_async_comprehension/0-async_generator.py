e14cb6d480455b7a3771645e344662a278d1b6f5
HamzaMoinet <moinethamza0@gmail.com>
2025-04-28 11:08:37 +0200

    add typing

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ attendant 1 seconde avant chaque valeur produite.
 """
 import asyncio
 import random
+import typing
 
 
-async def async_generator():
+async def async_generator() -> typing.Generator[float, None, None]:
     """
     Génère de manière asynchrone 10 nombres flottants aléatoires entre 0 et 10,
     en attendant 1 seconde avant de produire chaque valeur.
@@ -16,6 +17,6 @@ async def async_generator():
         float : Nombre aléatoire entre 0 et 10.
     """
 
-    for _ in range(10):
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
