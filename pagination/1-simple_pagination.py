6e1ae8bdb95f8d28724516a2a2c4359f9dfe3065
HamzaMoinet <moinethamza0@gmail.com>
2025-05-02 13:48:15 +0200

    replace def index_range

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -5,7 +5,7 @@ simple_pagination = __import__('1-simple_pagination').index_range
 
 def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
-    end_index = start_index+ page_size
+    end_index = page * page_size
     return (start_index, end_index)
 
 
