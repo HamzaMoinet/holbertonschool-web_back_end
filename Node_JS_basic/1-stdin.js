ae3f8fa4b559dca84c98ec6a2afcb7f1686bbdcb
HamzaMoinet <moinethamza0@gmail.com>
2025-05-21 10:08:51 +0200

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,11 +2,11 @@
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
   process.exit();
 });
